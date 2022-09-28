09a6b38592428114d151c8f372ea1ae95594f429
kirka1206 <askkirill@gmail.com>
2022-09-28 21:18:11 +0300

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -10,3 +10,8 @@ curl -o ~/.gitconfig https://raw.githubusercontent.com/kirka1206/dotfiles/main/.
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/kirka1206/dotfiles/main/.tmux.conf
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+#install et https://eternalterminal.dev/ - ssh без единого разрыва. не забыть запустить демон et на сервере :)
+apt install -y software-properties-common
+add-apt-repository ppa:jgmath2000/et
+apt update
+apt -y install et
